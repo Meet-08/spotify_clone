1ac50e6b357215ce1d73616da1ff850d798cdfbd
Meet-08 <meetjbhuva@gmail.com>
2025-06-05 00:25:56 +0530

    Implement authentication pages UI

---
+++ client/lib/features/auth/view/widgets/custom_field.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool isObscure;
+
+  const CustomField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isObscure = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(hintText: hintText),
+      controller: controller,
+      validator: (value) {
+        if (value!.trim().isEmpty) return "$hintText cannot be empty";
+        return null;
+      },
+      obscureText: isObscure,
+    );
+  }
+}
